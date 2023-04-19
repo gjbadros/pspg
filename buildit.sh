d9020b69b6d6eed35d7547c232f827b88a8035f8
Greg Badros <badros@gmail.com>
2023-04-19 10:14:39 -0700

    Add alternate brew locations for readline and link --force it

--- buildit.sh
+++ buildit.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -
-LDFLAGS="-L/usr/local/opt/readline/lib" CPPFLAGS="-I/usr/local/opt/readline/include" ./configure
-LDFLAGS="-L/usr/local/opt/readline/lib" CPPFLAGS="-I/usr/local/opt/readline/include" make
+brew link readline --force
+LDFLAGS="-L/usr/local/opt/readline/lib -L/opt/homebrew/opt/readline/lib" CPPFLAGS="-I/usr/local/opt/readline/include -I/opt/homebrew/opt/readline/include" ./configure
+LDFLAGS="-L/usr/local/opt/readline/lib -L/opt/homebrew/opt/readline/lib" CPPFLAGS="-I/usr/local/opt/readline/include -I/opt/homebrew/opt/readline/include" make
